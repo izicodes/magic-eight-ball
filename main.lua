363fa86a6dbb8e42f1e1513531ea3b26ee0c9f99
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-03-07 20:38:45 +0000

    🎱 Else-if statements added!

--- main.lua
+++ main.lua
@@ -10,4 +10,12 @@ local answer = ""
 -- Control Flow
 if number == 1 then
   answer = "Without a doubt."
+elseif number == 2 then
+  answer = "You must see for yourself."
+elseif number == 3 then
+  answer = "Yes!"
+elseif number == 4 then
+  answer = "It must be so."
+elseif number == 5 then
+  answer = "I can't say for sure."
 end
